8f60f8a958263d0dcd3fea8fd365d1203e06ab8b
Nicholas Montanez <nicholasmontanez@Nicholass-MacBook-Pro.local>
2019-03-31 22:36:44 -0400

    final commit

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: '8889',
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+    connection = mysql.createConnection({
+  host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'burgers_db'
 });
-
-
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -16,6 +19,4 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
-
 module.exports = connection;
